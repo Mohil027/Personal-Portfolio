eb8c01fa8f1316e8ad059b71d3d2df8567dc6184
Mohil Khimani <mohil.khimani@gmail.com>
2024-04-13 10:07:28 -0700

    updated project links

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -35,11 +35,11 @@ const Features = () => {
           <>
           <div className="card-row">
             <Card projectName="AutoVision: Web-ML Vehicle Recognition" imageUrl={img} description={"AI-driven system predicting vehicle type and model using Deep Learning and Web Development."} githubLink="https://github.com/Mohil027/vehicle-recognition" />
-            <Card projectName="Dynamic Demographic Insights Platform" imageUrl={img2} description={"Insightful demographics data visualization platform for demographic insights, citizen behaviors, and business analytics."} githubLink="url_to_github_project2" />
+            <Card projectName="Dynamic Demographic Insights Platform" imageUrl={img2} description={"Insightful demographics data visualization platform for demographic insights, citizen behaviors, and business analytics."} githubLink="https://github.com/Mohil027/Dynamic-Demographic-Insight-Platform" />
             <Card projectName="Full Stack Food Ordering Website" imageUrl={img3} description="Empowering Seamless Dining Experiences: Harnessing MERN Technology for End-to-End Food Ordering Innovation." githubLink="url_to_github_project3" />
           </div>
           <div className="card-row"> 
-            <Card projectName="Twitter + Youtube : A Backend" imageUrl={img} description="A social media backend leveraging Express.js and MongoDB to provide twitter and youtube features in a single app." githubLink="https://github.com/Mohil027/vehicle-recognition" />
+            <Card projectName="Twitter + Youtube : A Backend" imageUrl={img} description="A social media backend leveraging Express.js and MongoDB to provide twitter and youtube features in a single app." githubLink="https://github.com/Mohil027/Decentralized_social_media_backend" />
             {/* <Card projectName="Fake Reviews Detection System" imageUrl={img2} description="Project description 2" githubLink="url_to_github_project2" /> */}
             {/* <Card projectName="AirQ: India’s Air Quality Analysis" imageUrl={img3} description="Project description 3" githubLink="url_to_github_project3" /> */}
           </div>
@@ -50,12 +50,12 @@ const Features = () => {
         {activeTab === 'section2' && (
           <>
           <div className="card-row">
-            <Card projectName="Fake Reviews Detection System" imageUrl={img} description="Unveiling the Truth: Achieving 95% Accuracy in Fake Review Detection with PyTorch, GPT, and Bert." githubLink="url_to_github_project4" />
-            <Card projectName="Inverse Scaling Less GPT2" imageUrl={img2} description="Unleashing Precision: Harnessing Attention Masking for Inverse Scaling in GPT-2 Models." githubLink="url_to_github_project5" />
+            <Card projectName="Fake Reviews Detection System" imageUrl={img} description="Unveiling the Truth: Achieving 95% Accuracy in Fake Review Detection with PyTorch, GPT, and Bert." githubLink="https://github.com/Mohil027/Fake-Reviews-Classification" />
+            <Card projectName="Inverse Scaling Less GPT2" imageUrl={img2} description="Unleashing Precision: Harnessing Attention Masking for Inverse Scaling in GPT-2 Models." githubLink="https://github.com/Mohil027/Gpt2-inverse_scaling_solution" />
             <Card projectName="AutoVision: Web-ML Vehicle Recognition" imageUrl={img3} description={"AI-driven system predicting vehicle type and model using Deep Learning and Web Development."} githubLink="https://github.com/Mohil027/vehicle-recognition" />
           </div>
           <div className="card-row">
-            <Card projectName="Mammograms Colorization" imageUrl={img} description="Enhancing Early Detection: Automating Breast Cancer Diagnosis with GAN-Based Mammogram Colorization for Improved Visualization and Accuracy." githubLink="url_to_github_project4" />
+            <Card projectName="Mammograms Colorization" imageUrl={img} description="Enhancing Early Detection: Automating Breast Cancer Diagnosis with GAN-Based Mammogram Colorization for Improved Visualization and Accuracy." githubLink="https://link.springer.com/chapter/10.1007/978-981-19-5037-7_2" />
             {/* <Card projectName="Inverse Scaling Less GPT2" imageUrl={img2} description="Project description 5" githubLink="url_to_github_project5" />
             <Card projectName="Vehicle Sign Detection System" imageUrl={img3} description="Project description 6" githubLink="url_to_github_project6" /> */}
           </div>
